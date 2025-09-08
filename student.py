6803214d623084a3cec497df8a9e113721549967
Jolz1609 <csa16-6138@colegiosantoantonio.com.br>
2025-09-07 23:52:13 -0300

    student.py2

--- student.py
+++ student.py
@@ -1,8 +1,29 @@
-# Leia uma linha com o número do cartão
+
 numero = input()
+def luhn(numero: str) -> bool:
+    
+    numero = numero.replace(" ", "")
+    
+    
+    digitos = [int(d) for d in numero[::-1]]
+    
+    soma = 0
+    for i, digito in enumerate(digitos):
+        if i % 2 == 0:  
+            
+            soma += digito
+        else:  
+            
+            dobro = digito * 2
+            if dobro > 9:
+                dobro -= 9  
+            soma += dobro
+    
+    return soma % 10 == 0
+  
+if luhn():
+    print("Cartão válido!")
+else:
+    print("Cartão inválido!")
 
-# TODO: implemente a verificação pelo algoritmo de Luhn
-# Siga as dicas do README.
 
-# Ao final, imprima exatamente:
-# print("Cartão válido")  ou  print("Cartão inválido")
